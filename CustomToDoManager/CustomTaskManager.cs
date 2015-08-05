a08bdc10f942c6bea4f1deeaa9f40383ac15efc6
anastasia-khoroshko <anastasia.khoroshko@gmail.com>
2015-08-05 18:06:02 +0300

    CustomToDoManager
    Refactoring

---
+++ CustomToDoManager/CustomTaskManager.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo.Infrastructure;
+using CustomToDoManager.SlowService;
+using CustomToDoManager.FastService;
+using System.Diagnostics;
+
+namespace CustomToDoManager
+{
+    public class CustomTaskManager : ToDo.Infrastructure.IToDoManager
+    {
+        private static Service1Client fastClient;
+        private static ToDoManagerClient slowClient;
+        private static List<IToDoItem> items;
+        private bool isAdded;
+        
+        public CustomTaskManager()
+        {
+            fastClient = new Service1Client();
+            slowClient = new ToDoManagerClient();
+            items = new List<IToDoItem>();
+            isAdded = false;
+        }
+        public async void CreateToDoItem(IToDoItem todo)
+        {
+            var item = new CustomTask()
+            {
+                Name = todo.Name,
+                UserId = todo.UserId,
+                ToDoId = todo.ToDoId,
+                IsCompleted = todo.IsCompleted
+            };
+            items.Add(item as IToDoItem);
+            isAdded = true;
+            await fastClient.SyncToDoItemsAsync(item);
+        }
+
+        public int CreateUser(string name)
+        {
+            if (items == null) items.Clear();
+            return slowClient.CreateUserAsync(name).Result;
+        }
+
+        public async void DeleteToDoItem(int todoItemId)
+        {
+            int index = items.FindIndex(i => i.ToDoId == todoItemId);
+            if (index != -1)
+                items.RemoveAt(index);
+            await fastClient.SyncDeleteItemAsync(todoItemId);
+        }
+
+        public List<IToDoItem> GetTodoList(int userId)
+        {
+            if (isAdded==true || items == null || items.Count() == 0)
+            {
+                var list = slowClient.GetTodoList(userId);
+                if (list != null || list.Count() != 0)
+                {
+                    var newList = list.Select(i => i.ToCustomTask());
+                    items=newList.Select(i => (IToDoItem)i).ToList();
+                    isAdded = false;
+                    return items;
+                }
+                else return null;
+            }
+            else return items;
+        }
+
+        public async void UpdateToDoItem(IToDoItem todo)
+        {
+            int index = items.FindIndex(i => i.ToDoId == todo.ToDoId);
+            if (index != -1)
+            {
+                items.RemoveAt(index);
+                items.Add(todo);
+            }
+            await fastClient.SyncUpdateItemAsync((CustomTask)todo);
+        }
+
+        
+            
+
+    }
+}
---
+++ CustomToDoManager/Mapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomToDoManager.FastService;
+
+namespace CustomToDoManager
+{
+    public static class Mapper
+    {
+        public static CustomTask ToCustomTask(this CustomToDoManager.SlowService.ToDoItem item)
+        {
+            return new CustomTask()
+            {
+                Name = item.Name,
+                UserId = item.UserId,
+                ToDoId = item.ToDoId,
+                IsCompleted = item.IsCompleted
+            };
+        }
+
+        public static CustomToDoManager.SlowService.ToDoItem ToToDoItem(this CustomTask item)
+        {
+            return new CustomToDoManager.SlowService.ToDoItem()
+            {
+                Name = item.Name,
+                UserId = item.UserId,
+                ToDoId = item.ToDoId,
+                IsCompleted = item.IsCompleted
+            };
+        }
+    }
+}
